cf4b90a61d4339046a3000cc2280ff909622e2de
2510juan <98272914+2510juan@users.noreply.github.com>
2022-01-23 18:19:54 +0200

    Uploaded files from local drive

---
+++ App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function App() {
+
+  const [age, setAge] = useState('');
+  const [lowerLimit, setLowerLimit] = useState(0);
+  const [upperLimit, setUpperLimit] = useState(0);
+
+
+  
+  function change(text) {
+    setAge(text);
+    const lowerLimitResult = (220-text) * 0.65;
+    setLowerLimit(lowerLimitResult);
+    
+    const upperLimitResult = (220-text) * 0.85;
+    setUpperLimit(upperLimitResult);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.fields}>Age</Text>
+     
+      <TextInput style={styles.field} value={age}
+        onChangeText={text => change(text)}  keyboard-type='decimal-pad'>
+      </TextInput>
+      <Text style={styles.field}>HR limit</Text>
+      
+      <Text style={styles.field}>{lowerLimit.toFixed(0)} - {upperLimit.toFixed(0)}</Text>
+     
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 50,
+    marginLeft: 10
+  },
+  field: {
+    marginBottom: 10
+  }
+});
